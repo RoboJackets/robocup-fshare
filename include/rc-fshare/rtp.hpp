c46d3c5e623b1008a71a3a59ee7fe3c1e6273c5c
Zach Olkin <zach.olkin@gmail.com>
2019-07-03 16:10:49 +1000

    18 debugging messages

--- include/rc-fshare/rtp.hpp
+++ include/rc-fshare/rtp.hpp
@@ -108,7 +108,7 @@ struct RobotStatusMessage {
     unsigned kickStatus : 1;       // 0 = uncharged, 1 = charged
     unsigned kickHealthy : 1;      // 0 = unhealthy, 1 = healthy
     unsigned fpgaStatus : 1;       // 0 = good, 1 = error
-    int16_t encDeltas[14];         // encoder changes since last packet
+    int16_t encDeltas[18];         // encoder changes since last packet
 } __attribute__((packed));
 
 // Packet sizes
